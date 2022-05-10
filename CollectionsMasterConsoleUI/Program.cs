3f41711808a9b26cbff0bc7699f28fe4c5e767bd
John Ward <59837086+johndward01@users.noreply.github.com>
2022-05-10 17:46:29 -0500

    Update Program.cs

--- CollectionsMasterConsoleUI/Program.cs
+++ CollectionsMasterConsoleUI/Program.cs
@@ -9,27 +9,29 @@ namespace CollectionsMasterConsoleUI
         {
             //TODO: Follow the steps provided in the comments under each region.
             //Make the console formatted to display each section well
-            //Utlilize the method stubs at the bottom for the methods you must create
+            //Utlilize the method stubs at the bottom for the methods you must create ⬇⬇⬇
 
             #region Arrays
-            // Create an integer Array of size 50
+            //TODO: Create an integer Array of size 50
             
 
-            //Create a method to populate the number array with 50 random numbers that are between 0 and 50
+            //TODO: Create a method to populate the number array with 50 random numbers that are between 0 and 50
             
 
-            //Print the first number of the array
+            //TODO: Print the first number of the array
 
-            //Print the last number of the array            
+            //TODO: Print the last number of the array            
 
             Console.WriteLine("All Numbers Original");
-            //Use this method to print out your numbers from arrays or lists
+            //UNCOMMENT this method to print out your numbers from arrays or lists
             //NumberPrinter();
             Console.WriteLine("-------------------");
 
-            //Reverse the contents of the array and then print the array out to the console.
+            //TODO: Reverse the contents of the array and then print the array out to the console.
             //Try for 2 different ways
-            /*     Hint: Array._____(); Create a custom method     */
+            /*  1) First way, using a custom method => Hint: Array._____(); 
+                2) Second way, Create a custom method (scroll to bottom of page to find ⬇⬇⬇)
+            */
 
             Console.WriteLine("All Numbers Reversed:");
 
@@ -37,13 +39,13 @@ namespace CollectionsMasterConsoleUI
 
             Console.WriteLine("-------------------");
 
-            //Create a method that will set numbers that are a multiple of 3 to zero then print to the console all numbers
+            //TODO: Create a method that will set numbers that are a multiple of 3 to zero then print to the console all numbers
             Console.WriteLine("Multiple of three = 0: ");
             
 
             Console.WriteLine("-------------------");
 
-            //Sort the array in order now
+            //TODO: Sort the array in order now
             /*      Hint: Array.____()      */
             Console.WriteLine("Sorted numbers:");
             
@@ -55,45 +57,45 @@ namespace CollectionsMasterConsoleUI
             Console.WriteLine("************Start Lists**************");
 
             /*   Set Up   */
-            //Create an integer List
+            //TODO: Create an integer List
             
 
-            //Print the capacity of the list to the console
+            //TODO: Print the capacity of the list to the console
             
 
-            //Populate the List with 50 random numbers between 0 and 50 you will need a method for this            
+            //TODO: Populate the List with 50 random numbers between 0 and 50 you will need a method for this            
             
 
-            //Print the new capacity
+            //TODO: Print the new capacity
             
 
             Console.WriteLine("---------------------");
 
-            //Create a method that prints if a user number is present in the list
+            //TODO: Create a method that prints if a user number is present in the list
             //Remember: What if the user types "abc" accident your app should handle that!
             Console.WriteLine("What number will you search for in the number list?");
             
             Console.WriteLine("-------------------");
 
             Console.WriteLine("All Numbers:");
-            //Print all numbers in the list
+            //UNCOMMENT this method to print out your numbers from arrays or lists
             //NumberPrinter();
             Console.WriteLine("-------------------");
 
-            //Create a method that will remove all odd numbers from the list then print results
+            //TODO: Create a method that will remove all odd numbers from the list then print results
             Console.WriteLine("Odds Only!!");
             
             Console.WriteLine("------------------");
 
-            //Sort the list then print results
+            //TODO: Sort the list then print results
             Console.WriteLine("Sorted Odds!!");
             
             Console.WriteLine("------------------");
 
-            //Convert the list to an array and store that into a variable
+            //TODO: Convert the list to an array and store that into a variable
             
 
-            //Clear the list
+            //TODO: Clear the list
             
 
             #endregion
